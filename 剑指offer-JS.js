e46ba015fa64f35fc4f31429f03a2cde16a06304
Yin <Alexx_Yin@163.com>
2019-01-19 18:27:49 +0800

    sword to offer

--- 剑指offer-JS.js
+++ 剑指offer-JS.js
@@ -48,3 +48,18 @@ function printListFromTailToHead(head)
 }
 
 
+/*用两个栈实现队列
+题目描述
+用两个栈来实现一个队列，完成队列的Push和Pop操作。 队列中的元素为int类型。*/
+var stack1=[];
+function push(node)
+{
+    // write code here
+    stack1.push(node);
+}
+function pop()
+{
+    // write code here
+    return stack1.shift();
+}
+
