4a830dd3f7fcee2b156a42b45e43e18026591074
Yin <Alexx_Yin@163.com>
2019-01-20 21:40:56 +0800

    sword to offer

--- 剑指offer-JS.js
+++ 剑指offer-JS.js
@@ -63,3 +63,50 @@ function pop()
     return stack1.shift();
 }
 
+/*旋转数组的最小数字
+题目描述
+把一个数组最开始的若干个元素搬到数组的末尾，我们称之为数组的旋转。 输入一个非减排序的数组的一个旋转，
+输出旋转数组的最小元素。 例如数组{3,4,5,1,2}为{1,2,3,4,5}的一个旋转，该数组的最小值为1。 
+NOTE：给出的所有元素都大于0，若数组大小为0，请返回0。
+*/
+
+function minNumberInRotateArray(arr)
+{
+    //二分查找
+    let len = arr.length;
+    if(len == 0)  return 0;
+    let low = 0, high = len - 1;
+    while(low < high) {
+        let mid = low + Math.floor((high-low)/2);
+        if(arr[mid] > arr[high]) {
+            low = mid + 1;
+        } else if(arr[mid] == arr[high]) {
+            high = high - 1;
+        } else {
+            high = mid;
+        }
+    }
+ 
+    return arr[low];
+}
+
+
+/*斐波那契数列
+题目描述
+大家都知道斐波那契数列，现在要求输入一个整数n，请你输出斐波那契数列的第n项（从0开始，第0项为0）。
+n<=39*/
+
+function Fibonacci(n)
+{
+  if (n == 0) {
+    return 0;
+  }
+  var pre = 0, // 前一个值
+    cur = 1; // 当前值
+  for (var i = 2; i <= n; i++) { // 更新两个值
+    cur += pre;
+    pre = cur - pre;
+  }
+  return cur;
+}
+
