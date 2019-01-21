2de9bed2d50ce3c3ef14baed32fe66398ed9afa2
Yin <Alexx_Yin@163.com>
2019-01-21 23:14:48 +0800

    sword to offer

--- 剑指offer-JS.js
+++ 剑指offer-JS.js
@@ -164,3 +164,21 @@ function jumpFloorII(number)
     }
     return i;
 }
+
+/*矩形覆盖
+题目描述
+我们可以用2*1的小矩形横着或者竖着去覆盖更大的矩形。请问用n个2*1的小矩形无重叠地覆盖一个2*n的大矩形，总共有多少种方法？*/
+
+function rectCover(number)
+{
+    // write code here
+        if(number == 0){
+        return 0;
+    }
+    var result = [1,1];
+    for(var i=2;i<=number;i++){
+        result[i] = result[i-1]+result[i-2];
+    }
+    return result[number];
+}
+
