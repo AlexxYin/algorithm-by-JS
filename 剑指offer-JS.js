102d550a4dea1a99a08583d4f5efe6d204f2b7bf
Yin <Alexx_Yin@163.com>
2019-01-21 23:16:14 +0800

    sword to offer

--- 剑指offer-JS.js
+++ 剑指offer-JS.js
@@ -182,3 +182,17 @@ function rectCover(number)
     return result[number];
 }
 
+/*二进制中1的个数
+题目描述
+输入一个整数，输出该数二进制表示中1的个数。其中负数用补码表示。*/
+function NumberOf1(n)
+{
+    // write code here
+    var count = 0,flag=1;
+    while(flag){
+        if(n&flag)count++;
+        flag=flag<<1;
+    }
+    return count;
+}
+
