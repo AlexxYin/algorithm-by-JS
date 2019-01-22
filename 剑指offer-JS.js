de7b6228f64cf6727bc7925e9e395284682aec4d
Yin <Alexx_Yin@163.com>
2019-01-22 23:13:38 +0800

    sword to offer

--- 剑指offer-JS.js
+++ 剑指offer-JS.js
@@ -227,3 +227,22 @@ function reOrderArray(array)
    return jishu.concat(oushu);
 }
 
+
+/*链表中倒数第k个节点
+题目描述
+输入一个链表，输出该链表中倒数第k个结点。*/
+
+/*function ListNode(x){
+    this.val = x;
+    this.next = null;
+}*/
+function FindKthToTail(head, k)
+{
+    // write code here
+        var arr = [];
+    while(head!=null){
+        arr.push(head);
+        head = head.next;
+    }
+    return arr[arr.length-k];
+}
