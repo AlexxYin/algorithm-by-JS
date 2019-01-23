009d4fe1292ede9ccf4beaee92282b321a324c19
Yin <Alexx_Yin@163.com>
2019-01-23 22:43:00 +0800

    sword to offer

--- 剑指offer-JS.js
+++ 剑指offer-JS.js
@@ -271,3 +271,30 @@ function ReverseList(pHead)
     return pHead;
 }
 
+
+/*合并两个排序的链表
+题目描述
+输入两个单调递增的链表，输出两个链表合成后的链表，当然我们需要合成后的链表满足单调不减规则。*/
+
+/*function ListNode(x){
+    this.val = x;
+    this.next = null;
+}*/
+function Merge(pHead1, pHead2)
+{
+    // write code here
+        let list = {}
+    if(pHead1 === null){
+        return pHead2;
+    } else if (pHead2 === null) {
+        return pHead1;
+    }
+    if(pHead1 > pHead2){
+        list = pHead2;
+        list.next = Merge(pHead2.next, pHead1);
+    } else {
+        list = pHead1;
+        list.next = Merge(pHead2, pHead1.next)
+    }
+    return list;
+}
