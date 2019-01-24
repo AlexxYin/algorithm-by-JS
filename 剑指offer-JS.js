89348df2449df5ea1f8841af7b14b496f1d194a1
Yin <Alexx_Yin@163.com>
2019-01-24 22:46:29 +0800

    sword to offer

--- 剑指offer-JS.js
+++ 剑指offer-JS.js
@@ -378,3 +378,25 @@ function MoreThanHalfNum_Solution(numbers)
     return 0;
 }
 
+
+/*丑数
+题目描述
+把只包含质因子2、3和5的数称作丑数（Ugly Number）。
+例如6、8都是丑数，但14不是，因为它包含质因子7。 习惯上我们把1当做是第一个丑数。求按从小到大的顺序的第N个丑数。*/
+
+function GetUglyNumber_Solution(index)
+{
+    // write code here
+        if(index <= 0) return 0;
+    var count2 = 0,
+        count3 = 0,
+        count5 = 0;
+    var uglyNum = [1];
+    for(var i=1 ;i<index ;i++){
+        uglyNum[i] = Math.min(uglyNum[count2]*2,uglyNum[count3]*3,uglyNum[count5]*5);
+        if(uglyNum[i] == uglyNum[count2]*2) count2++;
+        if(uglyNum[i] == uglyNum[count3]*3) count3++;
+        if(uglyNum[i] == uglyNum[count5]*5) count5++;
+    }
+    return uglyNum[index-1];
+}
