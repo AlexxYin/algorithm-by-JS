eb3de9129257b8ed63e13de82c0b638cfb8a6fd5
Yin <Alexx_Yin@163.com>
2019-02-02 21:05:23 +0800

    sword to offer

--- 剑指offer-JS.js
+++ 剑指offer-JS.js
@@ -932,3 +932,20 @@ function Sum_Solution(n)
     (n>0)&&(res += Sum_Solution(n-1));
     return res;
 }
+
+
+/*不用加减乘除做加法
+题目描述
+写一个函数，求两个整数之和，要求在函数体内不得使用+、-、*、/四则运算符号。*/
+
+function Add(num1, num2)
+{
+    // write code here
+  let tmp = num1;
+  while (num2) {
+    tmp = num1 ^ num2;
+    num2 = (num2 & num1) << 1;
+    num1 = tmp;
+  }
+  return num1;
+}
