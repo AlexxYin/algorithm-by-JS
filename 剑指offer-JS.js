e5b6b31691d95d8fda9b21a5295f624f1bcfb03c
Yin <Alexx_Yin@163.com>
2019-02-26 21:35:20 +0800

    update2

--- 剑指offer-JS.js
+++ 剑指offer-JS.js
@@ -1238,3 +1238,38 @@ function duplicate(numbers, duplication)
     }
     return false;
 }
+
+
+
+/*二叉树的下一节点
+题目描述
+给定一个二叉树和其中的一个结点，请找出中序遍历顺序的下一个结点并且返回。注意，树中的结点不仅包含左右子结点，同时包含指向父结点的指针。*/
+
+
+/*function TreeLinkNode(x){
+    this.val = x;
+    this.left = null;
+    this.right = null;
+    this.next = null;
+}*/
+function GetNext(pNode)
+{
+    if(pNode===null)
+        return null;
+    // write code here
+    //如果该节点有右子树，那么该节点的下一个节点，就是右子树最左的节点
+    if(pNode.right){
+        let root=pNode.right;
+        while(root.left!==null){
+            root=root.left;
+        }
+        return root;
+    }
+    //没右子树，则找第一个当前节点是父节点左孩子的节点
+     while(pNode.next!==null){
+            if(pNode.next.left===pNode)
+                return pNode.next;
+            pNode = pNode.next;
+        }
+    return null;
+}
