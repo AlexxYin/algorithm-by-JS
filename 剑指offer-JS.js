28814066e656efcbc3b767acaecb1edf6d71eedc
Yin <Alexx_Yin@163.com>
2019-02-26 19:52:07 +0800

    update1

--- 剑指offer-JS.js
+++ 剑指offer-JS.js
@@ -1188,3 +1188,53 @@ function LastRemaining_Solution(n, m)
         return (LastRemaining_Solution(n - 1, m) + m) % n;
     }
 }
+
+
+/*数组中重复的数字*/
+/*题目描述
+在一个长度为n的数组里的所有数字都在0到n-1的范围内。
+ 数组中某些数字是重复的，但不知道有几个数字是重复的。
+ 也不知道每个数字重复几次。请找出数组中任意一个重复的数字。 
+ 例如，如果输入长度为7的数组{2,3,1,0,2,5,3}，那么对应的
+ 输出是第一个重复的数字2。*/
+
+ 数组排序法：
+ function duplicate(numbers, duplication)
+{
+    // write code here
+    //这里要特别注意~找到任意重复的一个值并赋值到duplication[0]
+    //函数返回True/False
+    let length = numbers.length;
+    if(numbers === null || length ===1){
+        return false;
+    }
+    let arr = numbers.sort((a,b) => a-b);
+    for(let i=0;i<length;i++){
+        if(arr[i]===arr[i+1]){
+            duplication[0]=numbers[i];
+            return true;
+        }
+    }
+    return false;
+}
+
+/*不修改数组找出重复的数字*/
+
+function duplicate(numbers, duplication)
+{
+    let length = numbers.length;
+    if(numbers === null || length ===1){
+        return false;
+    }
+    let arr = [];
+    for(let i=0;i<length;i++){
+        if(arr[numbers[i]]){
+            duplication[0] = arr[numbers[i]];
+            console.log(duplication[0]);
+            return true;
+        }else{
+            arr[numbers[i]] = numbers[i];
+        }
+    }
+    return false;
+}
